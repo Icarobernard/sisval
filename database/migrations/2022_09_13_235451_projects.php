98f861153f4937b5f2751ea4e801cdaa15a9a582
icarobernard <icarobernar@hotmail.com>
2022-09-13 20:57:03 -0300

    update migration

--- database/migrations/2022_09_13_235451_projects.php
+++ database/migrations/2022_09_13_235451_projects.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('responsible');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('projects');
     }
 };
