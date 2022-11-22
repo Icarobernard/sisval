28993d4ec11abc458d3d7d6057ca2e7b407a4f6b
icarobernard <icarobernar@hotmail.com>
2022-11-22 16:57:28 -0300

    add fcd structure

---
+++ app/Http/Controllers/FcdController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Fcd;
+use App\Models\Project;
+
+class FcdController extends Controller
+{
+    function create(Request $request)
+    {
+    }
+}
