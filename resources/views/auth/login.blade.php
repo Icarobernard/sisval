39ea689fcca00a659445b14281c73a4b55770a7e
icarobernard <icarobernar@hotmail.com>
2023-01-24 13:27:54 -0300

    add readme

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,9 +37,9 @@
                 <input type="email" name="email" class="form-control" placeholder="{{ __('Email...') }}" value="{{ old('email', '') }}" required>
               </div>
               @if ($errors->has('email'))
-                <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
-                  <strong>{{ $errors->first('email') }}</strong>
-                </div>
+              <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
+                <strong>{{ $errors->first('email') }}</strong>
+              </div>
               @endif
             </div>
             <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
@@ -52,9 +52,9 @@
                 <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Password...') }}" value="{{ !$errors->has('password') ? "" : "" }}" required>
               </div>
               @if ($errors->has('password'))
-                <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
-                  <strong>{{ $errors->first('password') }}</strong>
-                </div>
+              <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
+                <strong>{{ $errors->first('password') }}</strong>
+              </div>
               @endif
             </div>
             <div class="form-check mr-auto ml-3 mt-3">
@@ -73,16 +73,16 @@
       </form>
       <div class="row">
         <div class="col-6">
-            @if (Route::has('password.request'))
+          <!-- @if (Route::has('password.request'))
                 <a href="{{ route('password.request') }}" class="text-light">
                     <small>{{ __('Esqueceu a senha?') }}</small>
                 </a>
-            @endif
+            @endif -->
         </div>
         <div class="col-6 text-right">
-            <a href="{{ route('register') }}" class="text-light">
-                <small>{{ __('Criar nova conta') }}</small>
-            </a>
+          <a href="{{ route('register') }}" class="text-light">
+            <small>{{ __('Criar nova conta') }}</small>
+          </a>
         </div>
       </div>
     </div>
